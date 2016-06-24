c8862b56487188e3e6d6e4a0d0169fb851ee4569
Gabriel Santos <Spartan0799@users.noreply.github.com>
2016-06-24 11:54:35 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trabalho_PrimeNumbersList_Gabriel_Juan_2003
+{
+    class List
+    {
+        static void Main(string[] args)
+        {
+            int a = Convert.ToInt32(Console.ReadLine());
+            bool isPrime = true;
+
+            for (int i = 1; i <= a; i++)
+            {
+                for (int primas = 2; primas <= a; primas++)
+                {
+
+                    if (i != primas && i % primas == 0)
+                    {
+                        isPrime = false;
+
+                        break;
+                    }
+
+
+                }
+
+                if (isPrime)
+                {
+                    Console.WriteLine(+ i);
+                }
+
+                isPrime = true;
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
+
